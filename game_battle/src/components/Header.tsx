92fbd27c71e66f209d73cdb1902c001918f24326
ameralmonajed <a.munajjed@icloud.com>
2025-10-03 15:52:53 +0200

    create and add Footer

---
+++ game_battle/src/components/Header.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import React from 'react';
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+import { useState } from "react";
+
+
+
+
+export default function Header() {
+    return (
+        <header className="bg-slate-900 text-white shadow-md">
+        head 
+        </header>
+    )
+}
