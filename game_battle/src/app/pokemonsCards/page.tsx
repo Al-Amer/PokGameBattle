f8ad8d12f093193f7f09bbd13ff33b92cbd036f5
ameralmonajed <a.munajjed@icloud.com>
2025-10-05 16:32:06 +0200

    app/pokemonsCards/page.tsx fetch and show pokem in list (li)

--- game_battle/src/app/pokemonsCards/page.tsx
+++ game_battle/src/app/pokemonsCards/page.tsx
@@ -1,32 +1,51 @@
 'use client'
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
-
-
-
-
-
-export default function PokemonsCards() {
-const [selectedPokemon, setSelectedPokemon] = useState(null)
-
-const getPokemon = async () => {
-  const res = await fetch("https://pokeapi.co/api/v2");
-  if (!res.ok) {
-    console.error(`Error fetching Pokemon: ${res.statusText}`);
-    return 
-  }
-  const poke = await res.json();
-  setSelectedPokemon(poke);
+interface Pokemon{
+  name:string;
+  url:string;
 }
 
-console.log(getPokemon);  return (
-    <div>
-      
+export default function PokemonsCards() : React.FC {
+// const PokemonsCards: React.FC = () => {
+const [selectedPokemon, setSelectedPokemon] = useState<Pokemon[] | null>(null);
 
+const getPokemon = async () => {
+  try{
+    //fetch 
+    const res = await fetch("https://pokeapi.co/api/v2/pokemon");
+    if (!res.ok) {
+    console.error(`Error fetching Pokemon: ${res.statusText}`);
+    return 
+    }
+    const pokemones = await res.json();
+  setSelectedPokemon(pokemones.results);
+  // Error catching and handling
+  }catch(error){
+      console.error("Fetch failed:", error);
+  }
+};
+  useEffect(()=>{
+    getPokemon();
+  },[]);
+  
+  return (
+    <div>
+        <h1>Pokémon List</h1>
+      {selectedPokemon ? (
+        <ul>
+          {selectedPokemon.map((p) => (
+            <li key={p.name}>{p.name}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   )
 }
+// export default PokemonsCards;
 
 
 
